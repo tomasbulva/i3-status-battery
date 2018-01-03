2f1f32cdcdf4a38c9f7918ab26823dbcea545eb9
Tomas Bulva <megarameno@gmail.com>
2018-01-03 07:38:17 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 import { EventEmitter } from 'events';
+import linuxBattery from 'linux-battery';
+import isCharging  from 'is-charging';
 
-export default class Text extends EventEmitter {
+export default class Battery extends EventEmitter {
+    
     constructor(options, output) {
         super();
         options = options || {};
@@ -12,15 +15,66 @@ export default class Text extends EventEmitter {
         this.text = options.text || '';
         this.secretValue = options.secretValue;
 
+        this.charging = false;
+        this.colors = {
+            charging: '#aded89',//'#e1fce0',
+            normal: '#e0e0e0',
+            warning: '#f75d42'
+        }
+
     }
 
     update() {
-        //update output
-        this.output.full_text = this.text;
-        this.output.short_text = this.text;
 
-        //emit updated event to i3Status
-        this.emit('updated', this, this.output);
+        this.emit('pause', this);
+        
+
+        isCharging()
+        .then(result => {
+            
+            this.charging = result
+
+            var full = '<span size="large"></span> ';
+            var threequaters = '<span size="large"></span> ';
+            var half = '<span size="large"></span> ';
+            var onequarter = '<span size="large"></span> ';
+            var empty = '<span size="large"></span> ';
+            var charging = '<span size="large"></span> ';
+
+            linuxBattery() 
+            .then( res => {
+                let perc = parseFloat(res[0].percentage.slice(0, res[0].percentage.length));
+
+                let percents = ' <span size="small"><sup>' + perc + '%</sup></span>';
+                var output = '';
+
+                switch(true) {
+                    case this.charging: // charging
+                        output = '<span color="'+this.colors.charging+'">' + charging + percents + '</span>';
+                    break;
+                    case (!this.charging && perc >= 90): // charging
+                        output = '<span color="'+this.colors.normal+'">' + full + percents + '</span>';
+                    break;
+                    case (!this.charging && perc >= 75): // charging
+                        output = '<span color="'+this.colors.normal+'">' + threequaters + percents + '</span>';
+                    break;
+                    case (!this.charging && perc >= 50): // charging
+                        output = '<span color="'+this.colors.normal+'">' + half + percents + '</span>';
+                    break;
+                    case (!this.charging && perc >= 15): // charging
+                        output = '<span color="'+this.colors.normal+'">' + onequarter + percents + '</span>';
+                    break;
+                    case (!this.charging && perc < 15): // charging
+                        output = '<span color="'+this.colors.warning+'">' + empty + percents + '</span>';
+                    break;
+                }
+                this.output.full_text = output;
+                this.output.short_text = output;
+
+                this.emit('resume', this);
+                this.emit('updated', this, this.output);
+            });
+        });
     }
 
     action(action) {
